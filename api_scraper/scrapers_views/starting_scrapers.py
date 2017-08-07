106c80a158d83d14a6986e4d3f3f359054868c7d
Senetskiy <dikov.yury@gmail.com>
2017-08-07 11:34:32 +0300

    change BASEDIR

--- api_scraper/scrapers_views/starting_scrapers.py
+++ api_scraper/scrapers_views/starting_scrapers.py
@@ -1,11 +1,11 @@
 import logging
-from scrapers.settings import BASE_DIR
+import os
 from scrapers_karavan import scrapers_karavan_page
 from scrapers_dafi import scrapers_dafi_page
 
 DAFI_PAGE = "http://kharkov.dafi.ua/mall-promo/"
 KARAVAN_PAGE = 'https://kharkov.karavan.com.ua/mtype/sales-ru/'
-
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 logging.basicConfig(
     filename='{}/scrapers_views/logs/scarper.log'.format(BASE_DIR),
     level=logging.DEBUG,
